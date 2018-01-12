f57057c0a0e5b52ea8050ea60cd007073f094c7b
BlueSkyer-25 <775154084@qq.com>
2018-01-12 18:20:59 +0800

    整理目录结构

--- JDLAnimationTabbar/Category/UITabBarController+tabBarController.h
+++ JDLAnimationTabbar/Category/UITabBarController+tabBarController.h
@@ -1,6 +1,6 @@
 //
 //  UITabBarController+tabBarController.h
-//  JDL_BaiSiBuDeJie
+//  JDL
 //
 //  Created by 胜炫电子 on 2018/1/12.
 //  Copyright © 2018年 BlueSkyer-25. All rights reserved.
--- JDLAnimationTabbar/CustomTabbar/JDLTabBar.m
+++ JDLAnimationTabbar/CustomTabbar/JDLTabBar.m
@@ -1,6 +1,6 @@
 //
 //  JDLTabBar.m
-//  JDL_BaiSiBuDeJie
+//  JDL
 //
 //  Created by 胜炫电子 on 2018/1/11.
 //  Copyright © 2018年 BlueSkyer-25. All rights reserved.
